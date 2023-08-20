fb4c42edbb5bca0008bbb38775b6c85ba3645633
David Lucas Flayban <davidflayban2@hotmail.com>
2023-08-20 11:53:30 -0300

    Configurando o React Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+//Pages
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
 
 function App() {
   return (
     <div className="Mini Blog">
-      <h1>Mini Blog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path ='/About' element={<About/>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
