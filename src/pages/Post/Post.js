5d87a3b819159c6967d54d4bd40dd4ae2f6f925f
David Lucas Flayban <davidflayban2@hotmail.com>
2023-08-29 16:14:40 -0300

    Exibindo dados do post individual e CSS

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -2,13 +2,28 @@ import styles from "./Post.module.css"
 
 //hooks
 import { useParams } from "react-router-dom"
+import { useFetchDocument } from "../../Hooks/useFetchDocument"
 
 const Post = () => {
   const {id} = useParams()
-
+  const {document: post} = useFetchDocument("posts", id)
   return (
-    <div className= {styles.post}>
-        <h1>Post {id}</h1>
+    <div className= {styles.post_container}>
+        {post && (
+            <>
+                <h1>{post.title}</h1>
+                <img src={post.image} alt={post.title}/>
+                <p>Por: {post.creatBy}</p>
+                <p>{post.body}</p>
+                <h3>Tags do post</h3>
+                <div className={styles.tags}>
+                    {post.tagsArray.map((tag)=>(
+                        <p key={tag} ><span>#</span>{tag}</p>
+                    ))}
+                </div>
+                
+            </>
+        )}
     </div>
   )
 }
