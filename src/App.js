0afd3ab3c6cb8c8488f3a895e9aae32c24465125
David Lucas Flayban <davidflayban2@hotmail.com>
2023-08-29 15:40:32 -0300

    criando funcionaldiade e exibindo resultados de busca

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
 import CreatPost from './pages/CreatPost/CreatPost'
 import DashBoard from './pages/DashBoard/DashBoard'
+import Search from './pages/Search/Search'
 
 //Components
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+
 
 
 
@@ -47,6 +49,7 @@ function App() {
             <Routes>
               <Route path ='/' element={<Home/>} />
               <Route path ='/About' element={<About/>}/>
+              <Route path ='/search' element={<Search/>}/>
               <Route path ='/Register' element={ !user ? <Register/> : <Navigate to = "/"/>} />
               <Route path ='/Login' element={ !user ? <Login/> : <Navigate to = "/"/>}/>
               <Route path ='/posts/creat' element={user ? <CreatPost/> : <Navigate to = "/Login"/>}/>              
---
+++ src/pages/Search/Search.js
@@ -0,0 +1,31 @@
+import styles from "./Search.module.css"
+//hooks
+import { useFetchDocuments } from '../../Hooks/useFetchDocuments'
+import { useQuery } from '../../Hooks/useQuery'
+
+//Components
+import PostDetail from '../../components/PostDetail'
+import { Link } from 'react-router-dom'
+const Search = () => {
+  const query = useQuery()
+  const search = query.get("q")
+  const {documents: posts} = useFetchDocuments("posts", search)
+  return (
+    <div className={styles.search_container}>
+        <h2>Search</h2>
+        <div className={styles.nopost}>
+            {posts && posts.length == 0 && (
+                <>
+                  <p>Nenhum dado encontrado</p>
+                  <Link to ="/" className ="btn btn-dark">Voltar</Link>
+                </>
+            )}
+            {posts && posts.map((post)=> (
+              <PostDetail key={post.id} post={post}/>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Search
